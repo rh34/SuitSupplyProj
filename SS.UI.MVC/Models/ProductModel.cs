809c31e93388c0c3646d533b51a8dd5bc340b6e4
akaya <akaya@HB-DEV-70>
2018-10-19 10:14:20 +0300

    product controller, views and model created

---
+++ SS.UI.MVC/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SS.UI.MVC.Models
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
